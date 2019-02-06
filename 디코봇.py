d72a559d79588e5e81c2dda764cf13deac91e8b3
kimbolbol3 <47376563+kimbolbol3@users.noreply.github.com>
2019-02-06 13:42:35 +0900

    Add files via upload

---
+++ 디코봇.py
@@ -0,0 +1,25 @@
+import discord
+import asyncio
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("login")
+    print(client.user.name)
+    print(client.user.id)
+    print("--------------")
+    await client.change_presence(game=discord.Game(name='!도움말 ㄱㄱ', type=1))
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!출석체크'):
+        await client.send_message(message.channel, ":heart: 오늘도 출석체크 감사합니다 :heart: ")       
+    if message.content.startswith('!도움말'):
+        await client.send_message(message.channel, "`아직 미게발이에용!(!출석체크)좀 부탁요`")    
+    if message.content.startswith('!추석날 이밴트'):
+        await client.send_message(message.channel, "`감사합니다 돈을 드릴게요.`:moneybag::moneybag::moneybag::moneybag::moneybag:`됐죠?`")    
+
+
+
+client.run("NTM5Nzc1Nzg4MTY2MjgzMjY1.DzHQ6g.oFhJbVSrXaIhaBt43p6ICQE8rJM")
