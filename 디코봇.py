bbf817accaf2325535455c06fbb51d2248b7e302
kimbolbol3 <47376563+kimbolbol3@users.noreply.github.com>
2019-02-06 14:07:26 +0900

    Update 디코봇.py

--- 디코봇.py
+++ 디코봇.py
@@ -16,11 +16,11 @@ async def on_message(message):
     if message.content.startswith('!출석체크'):
         await client.send_message(message.channel, ":heart: 오늘도 출석체크 감사합니다 :heart: ")       
     if message.content.startswith('!도움말'):
-        await client.send_message(message.channel, "`아직 미게발이에용!(!출석체크)좀 부탁요`")    
+        await client.send_message(message.channel, "`아직 미게발이에용!(!출석체크)(!펫 구워먹기)좀 부탁요`")    
     if message.content.startswith('!추석날 이밴트'):
         await client.send_message(message.channel, "`감사합니다 돈을 드릴게요.`:moneybag::moneybag::moneybag::moneybag::moneybag:`됐죠?`")
-    if message.content.startswith('!펫 소환하기'):
-        await client.send_message(message.channel, "펫은 무제한 소환가능합니다`kimeqmon이 나타낫다``잡을까?``말까`")     
+    if message.content.startswith('!펫 구워먹기'):
+        await client.send_message(message.channel, "맜잇는 먹빵")     
     
 
 
