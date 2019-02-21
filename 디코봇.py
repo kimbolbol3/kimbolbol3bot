8cce8602eb8fab456291b0357ab550a2e64de9a5
kimbolbol3 <47376563+kimbolbol3@users.noreply.github.com>
2019-02-22 07:02:45 +0900

    Update 디코봇.py

--- 디코봇.py
+++ 디코봇.py
@@ -15,38 +15,8 @@ async def on_ready():
 async def on_message(message):
     if message.content.startswith('!출석체크'):
         await client.send_message(message.channel, ":heart: 오늘도 출석체크 감사합니다 :heart: ")       
-    if message.content.startswith('!도움말'):
-        await client.send_message(message.channel, "`아직 미게발이에용!(!출석체크)좀 해주세요           `")    
-    if message.content.startswith('!도움말'):
-        await client.send_message(message.channel, "`(!펫 구워먹기           `") 
-    if message.content.startswith('!도움말'):
-        await client.send_message(message.channel, "`           `")   
-    if message.content.startswith('!도움말'):
-        await client.send_message(message.channel, "`           `")
-    if message.content.startswith('!도움말'):
-        await client.send_message(message.channel, "`           `")
-    if message.content.startswith('!도움말'):
-        await client.send_message(message.channel, "`           `")
-    if message.content.startswith('!도움말'):
-        await client.send_message(message.channel, "`           `")
-    if message.content.startswith('!도움말'):
-        await client.send_message(message.channel, "`by,adadadwsx`")       
-    if message.content.startswith('!펫 구워먹기'):
-        await client.send_message(message.channel, "@~~ 진짜 있는줄 암? ㅋㅋㅋ 여윽시 초딩임 ㅋㅋㅋ")  
-    if message.content.startswith('!도움말'):
-        await client.send_message(message.channel, "`           `")   
-    if message.content.startswith('!도움말'):
-        await client.send_message(message.channel, "`           `")
-    if message.content.startswith('!도움말'):
-        await client.send_message(message.channel, "`           `")
-    if message.content.startswith('!도움말'):
-        await client.send_message(message.channel, "`           `")
-    if message.content.startswith('!도움말'):
-        await client.send_message(message.channel, "`           `")
-    if message.content.startswith('!도움말'):
-        await client.send_message(message.channel, "`           `")
-    if message.content.startswith('!도움말'):
-        await client.send_message(message.channel, "`by,adadadwsx`")    
+    if message.content.startswith('!'):
+        await client.send_message(message.channel, " @~~ 테스트 ")  
     
 
 
